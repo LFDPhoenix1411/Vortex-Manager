72ba7b1c7e39cea76b617a5c6eea139bcf4c0076
LFDPhoenix1411 <ethanfranken01@outlook.com>
2023-04-24 20:46:09 +0200

    modified:   src/assets/js/Welcome.js 	modified:   src/public/About.html 	modified:   src/public/Home.html 	modified:   src/public/Tickets.html 	modified:   src/public/Users.html

--- src/assets/js/Welcome.js
+++ src/assets/js/Welcome.js
@@ -1,5 +1,5 @@
-const welcomePopup = document.getElementById("welcomeModal");
-const popup = new bootstrap.Modal(welcomePopup);
+const infoPopup = document.getElementById("infoPopup");
+const popup = new bootstrap.Modal(infoPopup);
 
 popup.show();
 
@@ -11,3 +11,33 @@ popup.show();
 // closeButton.addEventListener("click", () => {
 //   localStorage.setItem("Visited?", "true");
 // });
+
+const updatePage = document.getElementById("UpdateTab");
+
+fetch("https://api.github.com/repos/LFDPhoenix1411/Vortex-Manager/commits")
+  .then((response) => response.json())
+  .then((data) => {
+    for (let commit of data) {
+      const update = document.createElement("div");
+      update.innerHTML = `
+      <div class="container my-3">
+        <div class="card mb-3 bg-dark text-white">
+          <div class="row g-0">
+            <div class="col-md-8">
+              <div class="card-body pe-4" style="min-height: 158px;">
+                <div class="d-flex align-items-center">
+                  <h5 class="card-title mb-0">${commit.commit.message}</h5>
+                </div>
+                <p class="card-text my-3">SHA: ${commit.sha}</p>
+                <p class="card-text my-3">Author: ${commit.commit.author.name}</p>
+                <p class="card-text my-3">Date: ${commit.commit.author.date}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+
+      updatePage.appendChild(update);
+    }
+  })
+  .catch((error) => console.error(error));
